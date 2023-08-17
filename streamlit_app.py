b5c64133d4a3642b50f19dfea484c3e172cd1a34
kmaAppDev <142517135+kmaAppDev@users.noreply.github.com>
2023-08-17 11:08:02 -0400

    Update streamlit_app.py
    tyring to force an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title('My parents new healthy diner!  Woot!')
 streamlit.title('<b>Can I use html?</b>')
 
 streamlit.title('no processed foods here')
+
+streamlit.title2('Trying to force an error')
