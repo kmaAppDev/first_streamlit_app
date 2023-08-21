149682d605047b3528f3c6b1668660e904dfe0a5
kmaAppDev <142517135+kmaAppDev@users.noreply.github.com>
2023-08-21 12:54:41 -0400

    Update streamlit_app.py
    adding a text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', my_data_row)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
