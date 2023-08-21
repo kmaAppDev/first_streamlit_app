ab9fbb61d7bc53658560eff0692760e0122bb3ef
kmaAppDev <142517135+kmaAppDev@users.noreply.github.com>
2023-08-21 15:02:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         my_data_rows = my_cur.fetchall()
-         return my_data_rows
+        return my_data_rows
       
 
 
